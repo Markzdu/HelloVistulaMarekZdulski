35fbd11bbefbf6604772658381bc4007d924ef83
Markzdu <zdulski.mk@gmail.com>
2021-10-04 14:18:40 +0200

    Adds Creator Info and gtiHub link

--- src/pl/vistula/helloworldzdulski/HelloWorldZdulski.java
+++ src/pl/vistula/helloworldzdulski/HelloWorldZdulski.java
@@ -2,7 +2,9 @@ package pl.vistula.helloworldzdulski;
 
 public class HelloWorldZdulski {
     public static void main(String[] args) {
-        System.out.println("Mz");
+        System.out.println("My name is : Marek Zdulski");
+        System.out.println("gitHub repo: https://github.com/Markzdu");
+        System.out.println("THX for checking it out");
     }
 
 }
