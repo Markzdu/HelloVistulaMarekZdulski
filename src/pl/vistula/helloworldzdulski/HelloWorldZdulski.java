d972cb91aa65f944aa8e3d395ef58c6f099b925f
Markzdu <zdulski.mk@gmail.com>
2021-10-04 14:20:07 +0200

    Remove thx form

--- src/pl/vistula/helloworldzdulski/HelloWorldZdulski.java
+++ src/pl/vistula/helloworldzdulski/HelloWorldZdulski.java
@@ -4,7 +4,7 @@ public class HelloWorldZdulski {
     public static void main(String[] args) {
         System.out.println("My name is : Marek Zdulski");
         System.out.println("gitHub repo: https://github.com/Markzdu");
-        System.out.println("THX for checking it out");
+
     }
 
 }
